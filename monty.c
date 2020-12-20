e83001e7d0d2e4a84e6038e5dd582d0c3ecc45e0
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-20 23:16:39 +0000

    error free, functioning pop/push/pall/swap

--- monty.c
+++ monty.c
@@ -11,13 +11,11 @@ int n = 0;
 
 int main(int argc, char **argv)
 {
-	printf("We've done literally nothing\n");
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	printf("About to enter opcode loop\n");
 	opcode_loop(argv);
 	return (0);
 }
@@ -39,15 +37,12 @@ int opcode_loop(char **argv)
 	size_t buffsize;
 	FILE *fp = fopen(argv[1], "r");
 
-	printf("Entering opcode loop function\n");
 	if(!fp)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	printf("before first getline\n");
 	characters = getline(&linebuff, &buffsize, fp);
-	printf("Before getline while loop\n");
 	while (characters >= 0)
 	{
 		for (i = 0; linebuff[i] != '\0'; i++)
@@ -55,15 +50,16 @@ int opcode_loop(char **argv)
 			if (linebuff[i] == '\n' && linebuff[i + 1] == '\0')
 				linebuff[i] = '\0';
 		}
-		printf("linebuff is [%s]\n", linebuff);
 		line_number++;
 		tokenize(&stack, linebuff);
+		if (linebuff != NULL)
+			free(linebuff);
 		linebuff = NULL;
 		characters = getline(&linebuff, &buffsize, fp);
 	}
 	fclose(fp);
-	free(linebuff);
-	printf("after getline while loop\n");
+	if (linebuff != NULL)
+		free(linebuff);
 	free_stack(stack);
 	return (0);
 }
@@ -83,30 +79,27 @@ int tokenize(stack_tt **stack, char *line)
 	linebuff = strtok(line, " ");
 	if (!linebuff)
 	{
-		exit(0);
+		printf("Linebuff is Null\n");
+		return(0);
 	}
-	printf("linebuff set as [%s]\n", linebuff);
 	nbuff = linebuff;
-	while (nbuff != NULL)
+/*	while (nbuff != NULL)
 	{
-		nbuff = strtok(NULL, " ");
+*/		nbuff = strtok(NULL, " ");
 		if (nbuff != NULL)
 		{
-			printf("nbuff set as %s\n", nbuff);
 			n = atoi(nbuff);
-			printf("n set as %d\n", n);
 			b = opcode_finder(stack, linebuff);
 		}
 		else if (b != 1)
 			b = opcode_finder(stack, linebuff);
-	}
-	printf("before token frees\n");
-	if (nbuff)
+/*	}
+ */	printf("%s = [nbuff] and %s = [linebuff]\n", nbuff, linebuff);
+/*	if (nbuff)
 		free(nbuff);
 	if (linebuff)
 		free(linebuff);
-	printf("after token frees\n");
-	return (n);
+*/	return (n);
 }
 
 
@@ -144,20 +137,17 @@ int opcode_finder(stack_tt **stack, char *linebuff)
 		{"\0", NULL}
 	};
 
-	printf("Entered opcodes with linebuff [%s]\n", linebuff);
 	if (linebuff != NULL)
 	{
 		for (i = 0; arr[i].f; i++)
 		{
-			printf("arr[i].f is %s\n", arr[i].opcode);
 			if (strcmp(linebuff, arr[i].opcode) == 0)
 			{
-				printf("found a match\n");
 				arr[i].f(stack, line_number);
 				return (1);
 			}
 		}
 	}
-	printf("L%d: unknown instruction %s\n", line_number, linebuff);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, linebuff);
 	exit(EXIT_FAILURE);
 }
